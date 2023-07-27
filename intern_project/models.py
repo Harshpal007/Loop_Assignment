47d60b82c7c44b6099fcb4563758f5566b3255d8
harshpal <harshpal31221@gmail.com>
2023-07-27 13:03:33 +0530

    report model

--- intern_project/models.py
+++ intern_project/models.py
@@ -10,3 +10,7 @@ class menu_hours(models.Model):
 class time_zone(models.Model):
     store_id=models.BigIntegerField(primary_key=True)
     timezone_str=models.CharField(max_length=100)
+
+class Report(models.Model):
+    report_id = models.AutoField(primary_key=True)
+    csv_report_file = models.FileField(upload_to='csv_reports/')
