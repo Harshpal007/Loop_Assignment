fdb886461bfafaaaf178e8b92b730fe7a657744f
harshpal <harshpal31221@gmail.com>
2023-07-27 13:03:42 +0530

    cellery module

--- intern_project/settings.py
+++ intern_project/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'intern_project',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -62,6 +64,13 @@ DATABASES = {
 }
 
 ROOT_URLCONF = 'intern_project.urls'
+
+CELERY_BROKER_URL = 'redis://localhost:6379/0'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 TEMPLATES = [
     {
